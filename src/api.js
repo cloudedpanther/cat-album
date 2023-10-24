0e23a3c12850c2e8d1430526eecc435c0c5781d2
cloudedpanther <rickeykoh11@gmail.com>
2023-10-24 23:43:20 +0900

    add fetching function

--- src/api.js
+++ src/api.js
@@ -1,2 +1,10 @@
 export const API_END_POINT =
   'https://l9817xtkq3.execute-api.ap-northeast-2.amazonaws.com/dev/';
+
+export const fetchNextNodes = async (nodeId) => {
+  const res = await fetch(`${API_END_POINT}${nodeId || ''}`);
+
+  if (res.ok) return await res.json();
+
+  throw new Error(res.Error);
+};
