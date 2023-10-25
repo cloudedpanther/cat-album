f6cd20692ca10538fead52b4f4f606888081f209
cloudedpanther <rickeykoh11@gmail.com>
2023-10-25 17:49:07 +0900

    add re-fetching process to fetcher function

--- src/api.js
+++ src/api.js
@@ -2,9 +2,13 @@ export const API_END_POINT =
   'https://l9817xtkq3.execute-api.ap-northeast-2.amazonaws.com/dev/';
 
 export const fetchNextNodes = async (nodeId) => {
-  const res = await fetch(`${API_END_POINT}${nodeId === 'root' ? '' : nodeId}`);
+  try {
+    const res = await fetch(
+      `${API_END_POINT}${nodeId === 'root' ? '' : nodeId}`,
+    );
 
-  if (res.ok) return await res.json();
-
-  throw new Error(res.Error);
+    if (res.ok) return await res.json();
+  } catch (error) {
+    fetchNextNodes(nodeId);
+  }
 };
